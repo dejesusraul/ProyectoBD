883e150e06a32f35b46b226ae2b4baf4caa71edd
dejesusraul <118905166+dejesusraul@users.noreply.github.com>
2024-01-13 19:34:59 -0500

    fotos y arreglar
    arreglar tabla, lavalidacion de login, fondo,

--- src/estruct/visual/Principal.java
+++ src/estruct/visual/Principal.java
@@ -8,11 +8,15 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.table.DefaultTableModel;
 
 public class Principal extends JFrame {
 
 	private JPanel contentPane;
-	private JTable tableReport;
+	private JTable table;
 
 	
 	public Principal() {
@@ -30,8 +34,59 @@ public class Principal extends JFrame {
 		JButton BtnReport = new JButton("Report");
 		panel.add(BtnReport);
 		
-		tableReport = new JTable();
-		tableReport.setBounds(143, 0, 466, 457);
-		contentPane.add(tableReport);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(142, 0, 467, 457);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column", "New column", "New column"
+			}
+		));
+		table.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		scrollPane.setViewportView(table);
 	}
 }
--- src/estruct/visual/login.java
+++ src/estruct/visual/login.java
@@ -10,23 +10,30 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
 import javax.swing.ImageIcon;
+
 import java.awt.Label;
 import java.awt.Font;
 import java.awt.Color;
+import java.awt.Toolkit;
+
+import javax.swing.JPasswordField;
 
 public class login extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textFieldUserPass;
+	private JPasswordField textFieldUserPass;
 	private JTextField textFieldUsserName;
 	JLabel LblError = new JLabel("New label");
 	
 
 	
 	public login() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(login.class.getResource("/img/votacion.png")));
 		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 497, 412);
@@ -38,13 +45,18 @@ public class login extends JFrame {
 		JButton btnNewButton = new JButton("Acept");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(Verificy()) {
-					LblError.setVisible(false);
+				if(!Verificy()) {
+					LblError.setVisible(true);
+					textFieldUsserName.setText("");
+					textFieldUserPass.setText("");
+					
+					
+					
+				}else{
 					Principal windowsPirnicpal = new Principal();
 					windowsPirnicpal.setVisible(true);
-					
-					
-				} 
+					dispose();
+				}
 			}
 		});
 		btnNewButton.setBounds(247, 209, 89, 23);
@@ -60,7 +72,7 @@ public class login extends JFrame {
 		btnNewButton_1.setBounds(362, 209, 89, 23);
 		contentPane.add(btnNewButton_1);
 		
-		textFieldUserPass = new JTextField();
+		textFieldUserPass = new JPasswordField();
 		textFieldUserPass.setBounds(247, 130, 202, 20);
 		contentPane.add(textFieldUserPass);
 		textFieldUserPass.setColumns(10);
@@ -68,7 +80,7 @@ public class login extends JFrame {
 		JLabel lblNewLabel = new JLabel("Introducing Pass");
 		lblNewLabel.setBackground(Color.WHITE);
 		lblNewLabel.setForeground(Color.BLACK);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblNewLabel.setBounds(247, 115, 180, 14);
 		contentPane.add(lblNewLabel);
 		
@@ -78,39 +90,41 @@ public class login extends JFrame {
 		textFieldUsserName.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Introducing Name");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblNewLabel_1.setBounds(247, 69, 202, 14);
 		contentPane.add(lblNewLabel_1);
+		LblError.setFont(new Font("Tahoma", Font.BOLD, 14));
 		LblError.setForeground(Color.RED);
 		
 		
-		LblError.setBounds(259, 161, 212, 14);
+		LblError.setBounds(259, 161, 212, 23);
 		contentPane.add(LblError);
-		LblError.setVisible(false);
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\Cesar Escalona\\Documents\\proyectoBD\\src\\img\\bandera.png"));
-		lblNewLabel_2.setBounds(0, 0, 481, 373);
-		contentPane.add(lblNewLabel_2);
+		JLabel fondo = new JLabel("New label");
+		fondo.setIcon(new ImageIcon(login.class.getResource("/img/peque.png")));
+		fondo.setBounds(0, 0, 481, 373);
+		contentPane.add(fondo);
+		LblError.setVisible(false);
 	}
 	
 	public boolean Verificy() {
 		boolean bandera=false;
 		String nameUser=textFieldUsserName.getText();
+		@SuppressWarnings("deprecation")
 		String passUser=textFieldUserPass.getText();
 		if(nameUser.equals("Cesar")|| nameUser.equals("Raul")){
             if(passUser.equals("cesar123") || passUser.equals("raul123")){
                 bandera=true;
             } else {
-            	LblError.setText("El nombre es incorrecto");
+            	LblError.setText(" La contraseña es incorrecta");
             	LblError.setVisible(true);
             }
             
         }else {
-        	LblError.setText("La contraseña es incorrecta");
+        	LblError.setText("El nombre es incorrecto");
         	LblError.setVisible(true);
         }
 		
 		return bandera;
 	}
-	
 }
